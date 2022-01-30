d16b8928b1dd979a7214ce99bbe6e24d0086f036
Dinesh Parmar <dparmar1999.dp@gmail.com>
2022-01-30 19:36:40 +0530

    Now API key will be taken from Environment variable incase of Web app hosted on Heroku
    Signed-off-by: Dinesh Parmar <dparmar1999.dp@gmail.com>

--- lib/util/helpers.dart
+++ lib/util/helpers.dart
@@ -21,7 +21,7 @@ void showSimpleDialog(String? title, String? message) async => RM.navigate.toDia
 Dio buildDio() {
   final Dio dio = Dio();
   if (kDebugMode) dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
-  dio.interceptors.add(ApiKeyInterceptor(apiKey: API_KEY));
+  dio.interceptors.add(ApiKeyInterceptor(apiKey: const String.fromEnvironment('API_KEY', defaultValue: API_KEY)));
 
   return dio;
 }
